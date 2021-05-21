7df74a82f2524e4be18fe470621da781f53f59f3
0xa8c81519 <0xa8c81519@gmail.com>
2021-05-21 21:49:04 +0800

    some bug

--- projects/stake1-test/src/app/reward-info/reward-info.component.ts
+++ projects/stake1-test/src/app/reward-info/reward-info.component.ts
@@ -56,7 +56,7 @@ export class RewardInfoComponent implements OnInit {
     }
 
     getRewardAPY(): Promise<BigNumber> {
-        let BSTPrice = 0.03;// 0.03 usd / bst
+        let BSTPrice = 0.003;// 0.03 usd / bst
         let denominator = new BigNumber(10).exponentiatedBy(18);
         return this.boot.poolContract.balanceOf(this.boot.proxyContract.address).then(lpStakingStr => {
             let totalLpStaking = new BigNumber(lpStakingStr.toString()).div(denominator);
@@ -74,12 +74,22 @@ export class RewardInfoComponent implements OnInit {
                                     let pArr = new Array();
                                     pArr.push(this.boot.minterContract.proxyInfo(this.boot.proxyContract.address));
                                     pArr.push(this.boot.minterContract.totalAllocPoint());
-                                    return Promise.all(pArr).then(arr => {
-                                        let percent = new BigNumber(arr[0].allocPoint.toString()).div(new BigNumber(arr[1].toString()));
-                                        let rewardAmt = blocks.multipliedBy(tokenPerBlock).multipliedBy(percent);
+                                    pArr.push(this.boot.proxyContract.poolInfo(this.boot.chainConfig.contracts.pid));
+                                    pArr.push(this.boot.proxyContract.totalAllocPoint());
+                                    return this.boot.tokenContract.balanceOf(this.boot.proxyContract.address).then(bstBalance => {
+                                        let rewardAmt = new BigNumber(bstBalance.toString()).div(denominator);//.multipliedBy(percent);
                                         let interestRate = rewardAmt.multipliedBy(BSTPrice).div(totalLpStaking);
                                         return interestRate.div(365).plus(1).exponentiatedBy(365).minus(1);
                                     });
+                                    // return Promise.all(pArr).then(arr => {
+                                    // let percent = new BigNumber(arr[0].allocPoint.toString()).div(new BigNumber(arr[1].toString()));//.multipliedBy(arr[2].allocPoint.toString()).div(arr[3].toString());
+                                    // let rewardAmt = blocks.multipliedBy(tokenPerBlock).multipliedBy(percent);
+                                    // return this.boot.tokenContract.balanceOf(this.boot.proxyContract.address).then(bstBalance => {
+                                    //     let rewardAmt = new BigNumber(bstBalance.toString()).div(denominator);//.multipliedBy(percent);
+                                    //     let interestRate = rewardAmt.multipliedBy(BSTPrice).div(totalLpStaking);
+                                    //     return interestRate.div(365).plus(1).exponentiatedBy(365).minus(1);
+                                    // });
+                                    // });
                                 });
                             });
                         } else {
