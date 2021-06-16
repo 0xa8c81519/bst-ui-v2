29550f18f835ac0b3e431b0664f0e2ad78d3e0c6
0xa8c81519 <0xa8c81519@gmail.com>
2021-06-17 07:19:06 +0800

    new version ng

--- projects/stake4-test/src/polyfills.ts
+++ projects/stake4-test/src/polyfills.ts
@@ -61,3 +61,10 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+ (window as any).process = {
+    env: { DEBUG: undefined },
+	browser:true
+};
+(window as any)['global'] = window;
+// @ts-ignore
+window.Buffer = window.Buffer || require('buffer').Buffer;
